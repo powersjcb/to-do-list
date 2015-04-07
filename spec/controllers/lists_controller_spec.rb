0af11e7a3816b817df0609e8ceae9d50adbcd59b
Jacob Powers <powersjcb@gmail.com>
2015-04-06 20:35:00 -0700

    Changed controller/user spec, switched to ssh on git

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ListsController, type: :controller do
 
-  it "should get specified list page" do
+  it "should get a user's list page" do
     pending
   end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  it "should get list of users pages" do
-    pending
-  end
+  # it "gets list of users page" do
+  #   get :index
+  #   expect(response).to render_template(:index)
+  # end
 
-  it "should get specfic user page" do
-    pending
-  end
+  # it "gets specfic user page" do
+  #   pending
+  # end
 end
